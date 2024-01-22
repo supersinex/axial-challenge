e52752f20f84892578f4c30756572e2095bddadf
Matthew Sinex <matthew.sinex@healthcatalyst.com>
2024-01-22 12:11:56 -0500

    chore: cleaned up some typos, updated constants, added some comments

--- src/app/features/financial-number/financial-number-input/financial-number-input.component.ts
+++ src/app/features/financial-number/financial-number-input/financial-number-input.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FinancialNumberUtilitiesService } from '../financial-number-utilities/financial-number-utilities.service';
+import { ROUTE_PATHS } from '../financial-number-routing.module';
 
 @Component({
   selector: 'app-financial-number-input',
@@ -19,13 +20,17 @@ export class FinancialNumberInputComponent {
   ) { }
 
   public handleSubmit(e: SubmitEvent): void {
+    /*
+     Submitting via the enter key will reload the page if an error occurs, but for this app, I felt it would be a better UX to
+     handle the errors without a reload, and display them back to the user.
+    */
     e.preventDefault();
     this.processInput();
   }
 
   public processInput(): void {
     if (this.financialNumber.valid) {
-      this._router.navigate(['../output'], { queryParams: { value: this.financialNumber.value }, relativeTo: this._activatedRoute });
+      this._router.navigate([`../${ROUTE_PATHS.output}`], { queryParams: { value: this.financialNumber.value }, relativeTo: this._activatedRoute });
     } else {
       this.financialNumber.markAsTouched();
     }
