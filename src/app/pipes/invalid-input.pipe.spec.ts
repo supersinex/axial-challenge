e6f619e053465fa4800a76f7ee89c81a316a85bc
Matthew Sinex <matthew.sinex@healthcatalyst.com>
2024-01-22 10:46:57 -0500

    test: unit tests for invalid input pipe

--- src/app/pipes/invalid-input.pipe.spec.ts
+++ src/app/pipes/invalid-input.pipe.spec.ts
@@ -5,4 +5,18 @@ describe('InvalidInputPipe', () => {
     const pipe = new InvalidInputPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return "Invalid input." when the input is falsy', () => {
+    const pipe = new InvalidInputPipe();
+    const input: unknown = undefined;
+    const result = pipe.transform(input);
+    expect(result).toBe('Invalid input.');
+  });
+
+  it('should return the input when it is truthy', () => {
+    const pipe = new InvalidInputPipe();
+    const input = 500;
+    const result = pipe.transform(input);
+    expect(result).toEqual(input);
+  });
 });
